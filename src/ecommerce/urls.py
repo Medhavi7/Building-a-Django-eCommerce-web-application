6bb63e9a31e7181246fee1b104dd882d3039c1a9
Medhavi <medharathod7@gmail.com>
2020-07-06 11:15:18 +0530

    Django eCommerce Web Application

---
+++ src/ecommerce/urls.py
@@ -0,0 +1,38 @@
+"""ecommerce URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib import admin
+from django.conf.urls import url
+
+from .views import home_page, about_page, contact_page, login_page, register_page
+
+urlpatterns = [
+    
+    url('about/', about_page),
+    url('contact/', contact_page),
+    url('login/', login_page),
+    url('register/', register_page),
+    url('', home_page),
+    url('admin/', admin.site.urls),
+]
+
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
