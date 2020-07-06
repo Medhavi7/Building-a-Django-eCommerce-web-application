7166bb7c7cf1f086e0e5c38a46d9bb5035fb9680
Medhavi <medharathod7@gmail.com>
2020-07-06 15:05:41 +0530

    Django eCommerce Web Application

--- src/ecommerce/s.py
+++
@@ -1,1 +0,0 @@
-SECRET_KEY = 'q!jea-h8r%2@hiszu31c%--29sh4rgk@_u&ge(l)#!$3i%g*m8'
--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import s
-
-
+from .creds import SECRET_KEY
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+# SECRET_KEY = creds.SECRET_KEY
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
