e600bcf9b9e671b11793d328010b4431ef4ec837
Medhavi <medharathod7@gmail.com>
2020-07-29 20:00:45 +0530

    Building-a-Django-eCommerce-web-application

--- src/products/views.py
+++ src/products/views.py
@@ -1,4 +1,3 @@
-#from django.views import ListView
 from django.http import Http404
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
@@ -48,7 +47,6 @@ class ProductDetailSlugView(DetailView):
     def get_object(self, *args, **kwargs):
         request=self.request
         slug=self.kwargs.get('slug')
-        #instance=get_object_or_404(Product, slug=slug, active=True)
         try:
             instance=Product.objects.get(slug=slug)
         except Product.DoesNotExist:
@@ -79,35 +77,12 @@ class ProductDetailView(DetailView):
             raise Http404("Product doesn't exist")
         return instance
 
-    #def get_queryset(self, *args, **kwargs):
-    #    request=self.request
-    #    pk=self.kwargs.get('pk')
-    #    return Product.objects.all()
+
 
 def product_detail_view(request,pk=None, *args, **kwargs):
-    #instance=Product.objects.get(pk=pk, featured=True)
-    #instance=get_object_or_404(Product, pk=pk)
-    #try:
-    #    instance = Product.objects.get(id=pk)
-    #except Product.DoesNotExist:
-    #    print('no product here')
-    #    raise Http404("Product doesn't exist")
-    #except:
-    #   print("huh?")
-
     instance=Product.objects.get_by_id(pk)
     if instance is None:
         raise Http404("Product doesn't exist")
-    #print(instance)
-
-    #qs = Product.objects.filter(id=pk)
-    #print(qs)
-    #if qs.exists() and qs.count==1:
-    #    instance=qs.first()
-    #else:
-    #    raise Http404("Product doesn't exist")
-
-
     context={
         'object' : instance 
     }
